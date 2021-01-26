69a90dc05839b6c83aea5fe90b145bde0f40e5e5
Uche Chilaka <uche@beam.dental>
2021-01-26 12:32:03 -0500

    feat: vendors/BaseCamp/components/Chart.js

---
+++ src/data/sample-01.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    color: 'gray',
+    description: 'Validation: Salesforce Integration',
+    x: 25,
+    size: 15
+  },
+  { color: 'maroon', description: 'Renewals', x: 80, size: 10 },
+  {
+    color: 'maroon',
+    description: 'Refactor: Fancy Pants',
+    x: 35,
+    size: 10
+  },
+  {
+    color: 'cyan',
+    description: 'Refactor: Lighthouse Orbs',
+    x: 45,
+    size: 10
+  },
+  {
+    color: 'yellow',
+    description: 'Migration & Legacy Data Updates',
+    x: 50,
+    size: 20
+  }
+];
---
+++ src/data/sample-02.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    color: 'red',
+    description: 'Validation: Salesforce Integration',
+    x: 25,
+    size: 15
+  },
+  { color: 'orange', description: 'Refactor: Renewals', x: 100, size: 10 },
+  {
+    color: '#adff2f',
+    description: 'Refactor: Complete Service',
+    x: 75,
+    size: 30
+  },
+  {
+    color: 'green',
+    description: 'Customizable Plan: Misc',
+    x: 90,
+    size: 20
+  }
+];
